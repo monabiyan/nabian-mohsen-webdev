b4da92865ff77070e8ba99b92ffb4f1dd5d0ffe7
monabiyan <monabiyan@gmail.com>
2016-10-23 20:24:51 -0400

    HW3 modified

--- public/assignment2/views/user/login.controller.client.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    angular
-        .module("WebAppMaker")
-        .controller("LoginController", LoginController);
-
-    function LoginController($scope) {
-        $scope.hello = "hello from login controller";
-    }
-})();
